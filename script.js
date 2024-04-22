d7242e5961a6aec2f46a8191518f8c7b6dcbca53
Arpit <dev.arpitparmar626@gmail.com>
2024-04-22 16:26:25 +0530

    minor changes in script js

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ const hero = document.querySelector(".hero")
 heroPara.innerHTML = `<p>Hello! I'm<br>Arpit Parmar</p>`
 hero.appendChild(heroPara)
 let textContents = [
-    `<p>I'M a<br>Web Developer</p>`,
-    `<p>Hello! I'M<br>Arpit Parmar</p>`
+    `<p>I'm a<br>Web Developer</p>`,
+    `<p>Hello! I'm<br>Arpit Parmar</p>`
 ]
 
 let count = 0;
